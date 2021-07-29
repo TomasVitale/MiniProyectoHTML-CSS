df7fae456eaa19447437c5850595ca7712496ace
TomasVitale <tomasvitale2@gmail.com>
2021-07-29 20:49:47 -0300

    Creando repositorio

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+app.get("/home",(req,res) => {
+    res.sendFile(path.join(__dirname,"views/home.html"))
+})
+app.listen(3000,() =>{
+    console.log("La app esta funcionando en http://localhost:3000");
+})
+app.use(express.static("public"));
