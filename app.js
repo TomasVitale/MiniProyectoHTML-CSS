3cf668eb2b865c0eb09080bf4da03850a491a1d8
TomasVitale <tomasvitale2@gmail.com>
2021-07-29 21:22:57 -0300

    configure puerto

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const path = require("path");
 app.get("/home",(req,res) => {
     res.sendFile(path.join(__dirname,"views/home.html"))
 })
-app.listen(3000,() =>{
+app.listen(process.env.PORT || 3000,() =>{
     console.log("La app esta funcionando en http://localhost:3000");
 })
 app.use(express.static("public"));
